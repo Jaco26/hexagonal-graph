423aa3a45e502e1cfbb2d96540e8d7efd9a8c723
Jacob Albright <jacob.albright23@gmail.com>
2020-02-08 13:29:43 -0600

    made cell highlighting richer with hover. code is confusing

--- main.js
+++ main.js
@@ -9,11 +9,13 @@ const board = new Board()
 const app = new Vue({
   template: `
   <div style="position: absolute; height: 100%; width: 100%; display: flex; align-items:center; justify-content: center">
-    <canvas ref="canvas" @click="onCanvasClick"></canvas>
+    <canvas ref="canvas" @click="onCanvasClick" v-on="lockSelected ? {} : { mousemove: onCanvasMousemove }"></canvas>
   </div>`,
   data() {
     return {
+      lockSelected: false,
       selectedCell: null,
+      hoveredCell: null,
       boardWidth: 0,
       boardHeight: 0,
       board: {
@@ -30,6 +32,13 @@ const app = new Vue({
     this.onResize()
   },
   methods: {
+    drawBoard() {
+      canvas._fillSelf()
+      board.grid.cells.forEach((cell, i) => {
+        canvas.arc({ ...cell.dimensions })
+        canvas.text({ ...cell.dimensions, fillStyle: 'black', text: i })
+      })
+    },
     onResize() {
       this.boardWidth = 750
       this.boardHeight = 750
@@ -39,20 +48,30 @@ const app = new Vue({
       }
       canvas.resize(this.boardWidth, this.boardHeight)
       board.resize(this.boardWidth, this.boardHeight)
-      this.drawGridOnCanvas()
-    },
-    drawGridOnCanvas() {
-      board.grid.cells.forEach((cell, i) => {
-        canvas.arc({ ...cell.dimensions })
-        canvas.text({ ...cell.dimensions, fillStyle: 'black', text: i })
-      })
+      this.drawBoard()
     },
     onCanvasClick(e) {
       const clicked = board.getCellByCoords(e.offsetX, e.offsetY)
-      this.selectedCell = this.selectedCell !== clicked ? clicked : null
-      canvas._fillSelf()
-      this.drawGridOnCanvas()
-      if (this.selectedCell) {
+      if (!this.lockSelected) {
+        this.selectedCell = clicked
+      } else {
+        this.selectedCell = null
+        this.hoveredCell = clicked
+      }
+      if (clicked || this.lockSelected) {
+        this.lockSelected = !this.lockSelected
+      }
+    },
+    onCanvasMousemove(e) {
+      if (!this.lockSelected) {
+        this.hoveredCell = board.getCellByCoords(e.offsetX, e.offsetY)
+      }
+    }
+  },
+  watch: {
+    selectedCell(newVal, oldVal) {
+      if (newVal) {
+        this.drawBoard()
         canvas.arc({ ...this.selectedCell.dimensions, fillStyle: 'purple' })
         canvas.text({ ...this.selectedCell.dimensions, text: this.selectedCell.id })
 
@@ -62,9 +81,25 @@ const app = new Vue({
           canvas.arc({ ...nbr.dimensions, fillStyle: 'lightblue' })
           canvas.text({ ...nbr.dimensions, fillStyle: 'black', text: nbr.id })
         })
-      }
+      } 
     },
-  },
+    hoveredCell(newVal, oldVal) {
+      if (newVal) {
+        this.drawBoard()
+        canvas.arc({ ...newVal.dimensions, fillStyle: '#a5a' })
+        canvas.text({ ...newVal.dimensions, text: newVal.id })
+
+        const nbrIds = board.adjacencyList.data[newVal.id]
+        Object.keys(nbrIds).forEach(direction => {
+          const nbr = board.grid.cells[nbrIds[direction]]
+          canvas.arc({ ...nbr.dimensions, fillStyle: '#44a5' })
+          canvas.text({ ...nbr.dimensions, fillStyle: 'black', text: nbr.id })
+        })
+      } else if (!newVal && oldVal) {
+        this.drawBoard()
+      }
+    }
+  }
 })
 
 app.$mount('#app')
